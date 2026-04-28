6216f41017ea9b3a9c9178850e5331cce1bff90f
Wadey3636 <wadey3636@gmail.com>
2026-04-28 16:58:33 -0700

    Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lua/config/binds.lua Add .config/nvim/lua/plugins/appearance/lualine.lua Add .config/nvim/lua/plugins/appearance/rainbow_delimiters.lua Add .config/nvim/lua/plugins/appearance/todo_comments.lua Add .config/nvim/lua/plugins/behavior_changes/cutlass.lua Add .config/nvim/lua/plugins/load_plugins.lua Add .config/nvim/lua/plugins/navigation/harpoon.lua Add .config/nvim/lua/plugins/navigation/oil.lua Add .config/nvim/lua/plugins/navigation/telescope.lua

---
+++ private_dot_config/nvim/init.lua
@@ -0,0 +1,31 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+require("plugins.load_plugins")
+require("config.binds")
+
+
+require('vim._core.ui2').enable()
+
+vim.opt.termguicolors = true
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("config-highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
+
+-- local treesitter = require("treesitter.treesitter_setup")
+-- treesitter.setup()
+
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.signcolumn = 'yes'
+vim.o.foldenable = false
+vim.o.wrap = false
+vim.wo.relativenumber = true
---
+++ private_dot_config/nvim/lua/config/binds.lua
@@ -0,0 +1,47 @@
+
+vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+
+
+-- Oil Binds
+vim.keymap.set("n", "<leader>e", "<cmd>Oil<cr>")
+
+-- Telescope Binds
+local builtin = require('telescope.builtin')
+
+vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>pg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>ps", function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+
+
+-- Harpoon Binds
+local harpoon = require("harpoon")
+
+harpoon:setup()
+vim.keymap.set("n", "m", function()
+  local key = vim.fn.getcharstr()
+  local list = harpoon:list()
+  local item = list.config.create_list_item(list.config)
+  list.items[key] = item
+  vim.notify("Bookmarked to: " .. key)
+end)
+
+vim.keymap.set("n", "<leader>j", function()
+  local key = vim.fn.getcharstr()
+  local list = harpoon:list()
+  local item = list.items[key]
+  if item then
+    list.config.select(item, list.config, nil)
+  else
+    vim.notify("No bookmark at: " .. key, vim.log.levels.WARN)
+  end
+end)
