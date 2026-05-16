e7cf6e471310ae24ea7e0fd03b1e3ea726591f9d
Wadey3636 <wadey3636@gmail.com>
2026-05-16 15:12:15 -0700

    Update .gtkrc-2.0 Update installs.sh

--- run_once_installs.sh
+++ run_once_installs.sh
@@ -3,6 +3,7 @@
 sudo pacman -S --needed git
 sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 sudo pacman -Syu
+pacman -S niri
 cd ~/
 
 source ./scripts/install_paru.sh
@@ -16,13 +17,26 @@ rustup component add rust-analyzer
 
 paru -S --needed --norebuild noctalia-shell vesktop 1password spicetify paru libadwaita-without-adwaita zen-browser-bin nordzy-icon-theme spotify
 
-if [ -d "/usr/share/sddm/themes/sddm-astronaut-theme" ]; then
-    echo "Astronaut theme installed"
-else
-    echo "Installing Astronaut theme"
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/keyitdev/sddm-astronaut-theme/master/setup.sh)"
-    rm -rf sddm-astronaut-theme
+if ! [ -d "/usr/share/sddm/themes/sddm-astronaut-theme" ]; then
+    while true; do
+        read -p "Do you wish to install Sddm Astronaut Theme? [y/n] " yn
+        case $yn in
+            [Yy]*)
+                echo "Installing Astronaut theme"
+                bash -c "$(curl -fsSL https://raw.githubusercontent.com/keyitdev/sddm-astronaut-theme/master/setup.sh)"
+                break
+                ;;
+            [Nn]*)
+                echo "Skipping Astronaut theme installation."
+                break
+                ;;
+            *)
+                echo "Please answer y or n."
+                ;;
+        esac
+    done
 fi
+
 
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
